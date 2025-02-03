63cb83f7eb352ab8eb08b846a2de507122fa1c26
Devesh Rathod <deveshrathod@Deveshs-MacBook-Air.local>
2025-02-04 04:21:18 +0530

    update for deployment 1

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,14 @@ import userRouter from "../backend/routes/user.route.js";
 import folderRouter from "./routes/data.route.js";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 dotenv.config();
 
 //DB connection
 connect();
+
+const __dirname = path.resolve();
 
 //init express app
 const app = express();
@@ -19,11 +22,17 @@ app.use(express.urlencoded({ extended: true }));
 //cors
 app.use(cors());
 
+//server
+app.listen(3000, () => {
+  console.log("server running");
+});
+
 //routes
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/data", folderRouter);
 
-//server
-app.listen(3000, () => {
-  console.log("server running");
+// Serve frontend
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
 });
