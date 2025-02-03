4371d996e2be6d276f7ddf6a9f86accae3d473c7
Devesh Rathod <deveshrathod@Deveshs-MacBook-Air.local>
2025-02-04 05:01:37 +0530

    update for deployment 12

--- backend/index.js
+++ backend/index.js
@@ -33,6 +33,7 @@ app.use("/api/v1/data", folderRouter);
 
 // Serve frontend
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
 });
